1b820c65992ef376afd905a2c41ba91d5ba8db47
Hdong27 <yhd9403@gmail.com>
2020-02-05 21:44:45 +0900

    reset the backend

---
+++ backend/findHome/src/test/java/com/example/findHome/FindHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
